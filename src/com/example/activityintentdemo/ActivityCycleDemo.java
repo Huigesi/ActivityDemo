9d9bf3bda38d490278fa2c8357662329b3a7ec6c
huigesi <791339970@qq.com>
2017-11-26 19:38:44 +0800

    生命周期的测试

---
+++ src/com/example/activityintentdemo/ActivityCycleDemo.java
@@ -0,0 +1,53 @@
+package com.example.activityintentdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class ActivityCycleDemo extends Activity{
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fragment_main);
+		Log.i("activity", "onCreate");
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		Log.i("activity", "onPause");
+	}
+
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+		Log.i("activity", "onRestart");
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		Log.i("activity", "onResume");
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		Log.i("activity", "onStart");
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.i("activity", "onStop");
+	}
+	
+
+}
